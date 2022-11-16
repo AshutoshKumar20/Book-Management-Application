0a512f5e5f5740bb4764a7b42384574d93e136fc
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2022-11-16 23:39:31 +0530

    Books section completed

--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,85 @@
 const express = require("express");
 
+const { books } = require("../data/books.json")
+const { users } = require("../data/users.json")
+
 const router = express.Router();
 
 /**
- * Route: /users
+ * Route: /books
  * Method: GET
- * Description: Getting all users
+ * Description: Get all books
  * Access: Public
  * Parameters: None
  */
+
+router.get("/", (req, res) => {
+    res.status(200).json({ success: true, data: books });
+});
+
+/**
+ * Route: /:id
+ * Method: GET
+ * Description: Getting all books by id
+ * Access: Public
+ * Parameters: id
+ */
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    const book = books.find((each) => each.id === id);
+
+    if (!book)
+        return res.status(404).json({
+            success: false,
+            message: "Book not found",
+        });
+
+    return res.status(200).json({
+        success: true,
+        data: book,
+    });
+});
+
+/**
+ * Route: /books/issued/by-user
+ * Method: GET
+ * Description: Get all issued books
+ * Access: Public
+ * Parameters: none
+ */
+
+router.get("/issued/by-user", (req, res) => {
+    const usersWithIssuedBooks = users.filter((each) => {
+        if (each.issuedBook) return each;
+    });
+
+    const issuedBooks = [];
+
+    usersWithIssuedBooks.forEach((each) => {
+
+        const book = books.find((book) => book.id === each.issuedBook);
+
+        book.issuedBy = each.issuedBy;
+        book.issuedDate = each.issuedDate;
+        book.returnDate = each.returnDate;
+
+        issuedBooks.push(book);
+
+    });
+    if (issuedBooks.length === 0)
+        return res.status(404).json({
+            success: false,
+            message: "No books issued yet",
+        });
+
+    return res.status(200).json({
+        success: true,
+        data: issuedBooks,
+    });
+});
+
+
 
 module.exports = router;
