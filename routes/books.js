bcd849b5a9c3b31da5e7d2ec362266c69546639d
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2023-01-08 19:32:21 +0530

    Completed Book Management Application

--- routes/books.js
+++ routes/books.js
@@ -43,7 +43,7 @@ router.get("/:id", (req, res) => {
 });
 
 /**
- * Route: /books/issued/by-user
+ * Route: /books/issued/by-user 
  * Method: GET
  * Description: Get all issued books
  * Access: Public
@@ -80,6 +80,75 @@ router.get("/issued/by-user", (req, res) => {
     });
 });
 
+/**
+ * Route: /books
+ * Method: POST
+ * Description: Create new book
+ * Access: Public
+ * Parameters: none
+ * Data: author, name, genre, price, publisher, id
+ */
 
+router.post("/", (req, res) => {
+    const { data } = req.body;
+
+    if (!data) {
+        return res.status(400).json({
+            success: false,
+            message: "No data provided",
+        });
+    }
+
+    const book = books.find((each) => each.id === data.id);
+
+    if (book) {
+        return res.status(404).json({
+            success: false,
+            message: "Book already exists with this id, please use a unique id",
+        });
+    }
+
+    const allBooks = [...books, data];
+
+    return res.status(201).json({
+        success: true,
+        message: allBooks,
+    });
+});
+
+/**
+ * Route: /books0/:id
+ * Method: PUT
+ * Description: Update book
+ * Access: Public
+ * Parameters: id
+ * Data: author, name, genre, price, publisher, id
+ */
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { data } = req.body;
+
+    const book = books.find((each) => each.id === id);
+
+    if (!book) {
+        return res.status(400).json({
+            success: false,
+            message: "Book not found with this particular id"
+        })
+    }
+
+    const updateData = books.map((each) => {
+        if (each.id === id) {
+            return { ...each, ...data };
+        }
+        return each;
+    });
+
+    return res.status(200).json({
+        success: true,
+        data: updateData,
+    });
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -140,4 +140,77 @@ router.delete("/:id", (req, res) => {
 
 });
 
+/**
+ * Route: /users/subscription-details/:id
+ * Method: DELETE
+ * Description: Get all user subscription details
+ * Access: Public
+ * Parameters: id
+ */
+
+router.get("/subscription-details/:id", (req, res) => {
+    const { id } = req.params;
+
+    const user = users.find((each) => each.id === id);
+
+    if (!user)
+        return res.status(404).json({
+            success: false,
+            message: "User not found",
+        });
+
+    const getDateInDays = (data = "") => {
+        let date;
+        if (data === "") {
+            // current date
+            date = new Date();
+        } else {
+            // getting date on basis of data variable
+            date = new Date(data);
+        }
+        let days = Math.floor(data / (1000 * 60 * 60 * 24));
+        return days;
+    };
+
+    const subscriptionType = (date) => {
+        if (user.subscriptionType === "Basic") {
+            date = date + 90;
+        } else if (user.subscriptionType === "Standard") {
+            date = date + 180;
+        } else if (user.subscriptionType === "Premium") {
+            date = date + 365;
+        }
+
+        return date;
+    };
+
+    //Subscription expiration caculation
+    //January 1, 1970, UTC. // milliseconds
+    let returnDate = getDateInDays(user.returnDate);
+    let currentDate = getDateInDays();
+    let subscriptionDate = getDateInDays(user.subscriptionDate);
+    let subscriptionExpiration = subscriptionType(subscriptionDate);
+
+    const data = {
+        ...user,
+        subscriptionExpired: subscriptionExpiration < currentDate,
+        daysLeftForExpiration:
+            subscriptionExpiration <= currentDate
+                ? 0
+                : subscriptionExpiration - currentDate,
+
+        fine:
+            returnDate < currentDate
+                ? subscriptionExpiration <= currentDate
+                    ? 200
+                    : 100
+                : 0,
+    };
+
+    res.status(200).json({
+        success: true,
+        data,
+    });
+});
+
 module.exports = router;
